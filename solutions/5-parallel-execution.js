a2330d31b2724e4f553694c08e1336f216c5a34d
Aleksey Eliseev <alekseyeliseev@ya.ru>
2024-04-29 16:41:38 +0300

    Начальный коммит

---
+++ solutions/5-parallel-execution.js
@@ -0,0 +1,8 @@
+import path from 'path';
+import fs from 'fs';
+import _ from 'lodash';
+import async from 'async';
+
+// BEGIN
+
+// END
---
+++ solutions/7-promises.js
@@ -0,0 +1,5 @@
+import fsp from 'fs/promises';
+
+// BEGIN
+
+// END
