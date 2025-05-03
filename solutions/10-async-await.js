1656ddba7ff9d806cb2577efbe949804e25010ca
temsas <artemkariakin1187@gmail.com>
2025-05-03 15:52:44 +0300

    Задание 6 выполнено

--- solutions/10-async-await.js
+++ solutions/10-async-await.js
@@ -1,5 +1,13 @@
 import fsp from 'fs/promises';
+import async from "async";
 
 // BEGIN
+const exchange = async (file1, file2) => {
+    const promise1 = await fsp.readFile(file1)
+    const promise2 = await fsp.readFile(file2)
 
+    await fsp.writeFile(file1, promise2)
+    await fsp.writeFile(file2, promise1)
+}
+export {exchange}
 // END
