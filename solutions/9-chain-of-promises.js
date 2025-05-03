3c691c471b181ded2e661b4b8ca464997ad92200
temsas <artemkariakin1187@gmail.com>
2025-05-03 15:44:45 +0300

    Задание 6 выполнено

--- solutions/9-chain-of-promises.js
+++ solutions/9-chain-of-promises.js
@@ -1,5 +1,19 @@
 import fsp from 'fs/promises';
 
 // BEGIN
-
+const getTypes = (paths) =>{
+    const promises = paths.map((path) =>{
+        return fsp.stat(path)
+            .then((stats) =>{
+                if(stats.isDirectory()){
+                    return 'directory'
+                } else {
+                    return 'file'
+                }
+            })
+            .catch(() => null)
+    })
+    return Promise.all(promises)
+}
+export {getTypes}
 // END
