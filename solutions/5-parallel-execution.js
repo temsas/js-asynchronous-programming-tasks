6ad0aed563a1b6474c8ec949900e28b766724e6e
temsas <artemkariakin1187@gmail.com>
2025-05-05 13:58:03 +0300

    Задание 6 выполнено

--- solutions/5-parallel-execution.js
+++ solutions/5-parallel-execution.js
@@ -1,8 +1,38 @@
 import path from 'path';
-import fs from 'fs';
+import fs, {stat} from 'fs';
 import _ from 'lodash';
 import async from 'async';
 
 // BEGIN
-
+const getDirectorySize = (directory, callback) => {
+    fs.readdir(directory, (error1, files) => {
+        if (error1) {
+            callback(error1)
+            return
+        }
+        async.map(files, (file, mapCallback) => {
+                const filePath = path.join(directory, file)
+                fs.stat(filePath, (statErr, stat) => {
+                    if (statErr) {
+                        mapCallback(statErr)
+                        return
+                    }
+                    if (stat.isFile()) {
+                        mapCallback(null, stat.size)
+                    } else {
+                        mapCallback(null, 0)
+                    }
+                })
+            },
+            (error3, sizes) => {
+                if (error3) {
+                    callback(error3)
+                    return
+                }
+                callback(null, _.sumBy(sizes))
+            }
+        )
+    })
+}
+export {getDirectorySize}
 // END
